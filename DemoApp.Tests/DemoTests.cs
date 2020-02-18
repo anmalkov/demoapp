4bc4e1a146ca648337ebb545ebd09a9f6f0bfd53
Andrew Malkov <anmalkov@microsoft.com>
2020-02-18 15:30:31 +0100

    Initial commit

---
+++ DemoApp.Tests/DemoTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DemoApp.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoApp.Tests
+{
+    [TestClass]
+    public class DemoTests
+    {
+        [TestMethod]
+        public void Index_ActionResult_should_be_not_null()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About_ActionResult_should_be_not_null()
+        {
+            var controller = new HomeController();
+
+            var result = controller.About();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Contact_ActionResult_should_be_not_null()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Contact();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ DemoApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemoApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1bc30e3-8c7f-45be-97b0-da81004b1e4c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
